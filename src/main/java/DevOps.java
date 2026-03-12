a5adcceb7f2491fcbb9c83cff34ec03730bfa7d5
Gayathrisadasivam-cse <gayathrisadasivam.cse@gmail.com>
2026-03-13 01:36:01 +0530

    2

--- src/main/java/DevOps.java
+++ src/main/java/DevOps.java
@@ -1,7 +1,7 @@
 public class DevOps {
 
     public static void main(String[] args) {
-        System.out.println("Hello CI/CD using Jenkins");
+        System.out.println("New CI build");
     }
 
 }
